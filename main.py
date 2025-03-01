5569fb7bf8f63d8e6992e470f8efd0232e8454b3
Jade Elliott <jelliott49@hawkmail.hccfl.edu>
2025-03-01 16:25:16 -0500

    Modified main.py to calculate and display state tax, federal tax, dependent deductions, total withholding, and take-home pay based on user input

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ dependentDeduction = salary * 0.025 *numDependents
 totalWithholding = stateTax + federalTax + dependentDeduction
 takeHomePay = salary - totalWithholding
 # output statements
+print("stateTax: $" + str(stateTax))
+print("federalTax: $" + str(federalTax))
+print("dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
