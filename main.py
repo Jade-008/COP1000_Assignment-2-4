5fb7984f433a3d943c43a108ea4bcf9e050df704
Jade Elliott <jelliott49@hawkmail.hccfl.edu>
2025-03-01 15:33:03 -0500

    Modified main.py to accept interactive input for salary and number of inputs

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = float(input("Enter your salary: "))
+numDependents = float(input("Enter the number of dependents: "))
 
 # calculate taxes here
-
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = salary * 0.025 *numDependents
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
 # output statements
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
